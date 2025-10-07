2a89d84599a04020bcae6b22ff4747d4bf529801
wate <110754234+WateTaks@users.noreply.github.com>
2025-10-07 10:53:01 -0300

    chore: removes old repositories unused config files, adds local deployment with docker compose
    also prepares the dockerfiles for upcoming production stage

---
+++ symcomp-server/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+echo "Waiting for database to be ready..."
+python wait-for-db.py
+
+# Apply database migrations (NOT RECOMMENDED TO PUT THE MAKEMIGRATIONS HERE)
+# But haven't found any other workaround
+echo "Creating and applying database migrations..."
+python manage.py makemigrations
+python manage.py migrate
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile,
+# or passed to the 'command' instruction in docker-compose.yml).
+exec "$@"
