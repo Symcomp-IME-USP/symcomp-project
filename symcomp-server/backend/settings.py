d5ee76e7b91bd7957706a1931ba09fa64168ce39
Xnths <jonathascastilhoes@gmail.com>
2025-10-10 22:08:40 -0300

    fix: ssl and csrf

--- symcomp-server/backend/settings.py
+++ symcomp-server/backend/settings.py
@@ -190,4 +190,10 @@ else:
         ],
     }
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://symcomp.ime.usp.br',
+]
 
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
