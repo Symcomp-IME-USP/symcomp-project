cdef666b39ee426ffe9da52ac6908ceae4ee0ac6
Xnths <jonathascastilhoes@gmail.com>
2025-10-11 09:54:26 -0300

    feat: adding google tracking and robots for site indexation

---
+++ symcomp/src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://symcomp.ime.usp.br',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://symcomp.ime.usp.br/semana',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ]
+}
